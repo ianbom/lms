94cc97ba0a6afd9e08e7f6bdf87813da3ebad985
Ian Ale <ianalebom@gmail.com>
2026-04-29 19:37:02 +0700

    ganti foto gero

--- resources/js/Components/Corporate/HeroSection.tsx
+++ resources/js/Components/Corporate/HeroSection.tsx
@@ -1,14 +1,11 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 
 const images = [
-    '/ImpactCompressed/fotonew.jpeg',
-    // '/ImpactCompressed/CSR, ESG and Sustainability Training.jpg',
-    '/ImpactCompressed/DSC00288.jpg',
-    '/ImpactCompressed/DSC03612.jpg',
-    '/ImpactCompressed/DSC00350.jpg',
-    // '/ImpactCompressed/DSC01305.jpg',
-    // '/ImpactCompressed/DSC08419.JPG',
-    // '/ImpactCompressed/presentasi.jpeg',
+    '/HeroImg/1.png',
+    '/HeroImg/3.png',
+    '/HeroImg/4.png',
+    '/HeroImg/5.png',
+    '/HeroImg/6.png',
 ];
 
 export default function HeroSection() {
